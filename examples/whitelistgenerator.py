45326706367fae5c6437f0c1ba35e5b110a775d0
anrisovich <34153344+anrisovich@users.noreply.github.com>
2017-12-03 20:47:20 +0300

    Update whitelistgenerator.py

--- examples/whitelistgenerator.py
+++ examples/whitelistgenerator.py
@@ -1,6 +1,4 @@
 """
-    instabot example
-
     Whitelist generator: generates a list of users which
     will not be unfollowed.
 """
@@ -16,11 +14,8 @@ from instabot import Bot
 bot = Bot()
 bot.login()
 
-wait = 5
+wait = 3
 
-print ("This script will generate whitelist.txt file with users"
-       "who will not be unfollowed by bot. "
-       "Press Y to add user to whitelist. Ctrl + C to exit.")
 your_following = bot.get_user_following(bot.user_id)
 already_whitelisted = bot.read_list_from_file("whitelist.txt")
 rest_users = list(set(your_following) - set(already_whitelisted))
@@ -29,7 +24,6 @@ with open("whitelist.txt", "a") as f:
     for user_id in rest_users:
         user_info = bot.get_user_info(user_id)
         print(user_info["username"])
-        print(user_info["full_name"])
 
         input_line = "y"
         if "y" in input_line:
